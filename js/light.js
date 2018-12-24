797e1484a10675f878f62760e0ee916350dd6276
khairulislam435 <khairul.winner@gmail.com>
2018-12-24 07:03:29 -0800

    PSD to HTML Convert

---
+++ js/light.js
@@ -0,0 +1,10 @@
+// lightbox.option({
+//     'resizeDuration': 2000,
+//     'wrapAround': true,
+//     'alwaysShowNavOnTouchDevices': true,
+//     'disableScrolling': true,
+//     'positionFormTop': 50,
+//     'showImageNumberLabel': false
+//
+// });
+
